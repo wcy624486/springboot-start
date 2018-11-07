f895baed6a3db6079898240df52245a7937b95a6
wucy <wucysj@yonyou.com>
2018-11-07 19:59:56 +0800

    thymeleaf

--- src/main/java/com/yonyou/controller/ThymeleafController.java
+++ src/main/java/com/yonyou/controller/ThymeleafController.java
@@ -1,8 +1,12 @@
 package com.yonyou.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yonyou.pojo.User;
 
 @Controller
 @RequestMapping("th")
@@ -17,6 +21,17 @@ public class ThymeleafController {
 	@RequestMapping("center")
 	public String center(){
 		return "thymeleaf/center/center";
+	}
+	
+	@RequestMapping("test")
+	public String test(ModelMap map){
+		User u = new User();
+		u.setName("qaz");
+		u.setAge(18);
+		u.setBirthday(new Date());
+		
+		map.addAttribute("user", u);
+		return "thymeleaf/test";
 	}
 
 }
